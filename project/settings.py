7452e4fab89529a60b902ddf76ab712d0b875d43
Zawidev <juttsaabjuttjee7796@gmail.com>
2024-08-03 15:04:47 +0500

    settings update

--- project/settings.py
+++ project/settings.py
@@ -36,7 +36,14 @@ SECRET_KEY = 'django-insecure-n@anrp9&m&)y$rlnecbeivu93=nu($230l0vqm4+4iwdaqt@82
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['cashquest-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://cashquest-production.up.railway.app']
+import os
+
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'cashquest-production.up.railway.app').split(',')
+
+CSRF_TRUSTED_ORIGINS = ['https://cashquest-production.up.railway.app']
+
 
 
 # Application definition
